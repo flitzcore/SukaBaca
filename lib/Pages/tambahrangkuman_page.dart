29b672ad422d6201ee81a011a30dc35247ba01d2
Krisna <krisnaerlangga08@gmail.com>
2022-03-19 03:56:44 -0700

    Progress

--- lib/Pages/tambahrangkuman_page.dart
+++ lib/Pages/tambahrangkuman_page.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-//import 'package:image_picker/image_picker.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as Path;
 import 'package:suka_baca/Pages/route_page.dart';
